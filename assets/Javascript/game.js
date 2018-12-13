8f393a87a3f80291b2de6cc00b09c5a387969063
david dobson <david.m.dobson@hotmail.com>
2018-12-12 19:35:48 -0500

    3rd

--- assets/Javascript/game.js
+++ assets/Javascript/game.js
@@ -9,33 +9,33 @@ $("#number-to-guess").append(generateRandomNumber(30, 100));
 //each crystal needs value
 let numberOptions = [10, 5, 3, 7];
 
-let counter= 0;
+let counter = 0;
 
 //for (let i = 0; i < numberOptions.length; i++) {
-   
+
 //}
 
 //each crystal clicked adds score
-$("#allCrystal").on("click", function() { 
- let imageCrystal=$(".crystal")
-    
-    $(imageCrystal).attr("data-crystalvalue", generateRandomNumber(1,5));
+$("#allCrystal").on("click", function () {
+    let imageCrystal = $(".crystal")
 
-    let imageCrystal2=$(".crystal2")
-    
-    $(imageCrystal2).attr("data-crystalvalue", generateRandomNumber(6,10));
-   
-    let imageCrystal3=$(".crystal3")
-    
-    $(imageCrystal3).attr("data-crystalvalue", generateRandomNumber(11,15));
+    $(imageCrystal).attr("data-crystalvalue", generateRandomNumber(1, 5));
 
-    let imageCrystal4=$(".crystal4")
-    
-    $(imageCrystal4).attr("data-crystalvalue", generateRandomNumber(16,18));
+    let imageCrystal2 = $(".crystal2")
+
+    $(imageCrystal2).attr("data-crystalvalue", generateRandomNumber(6, 10));
+
+    let imageCrystal3 = $(".crystal3")
+
+    $(imageCrystal3).attr("data-crystalvalue", generateRandomNumber(11, 15));
+
+    let imageCrystal4 = $(".crystal4")
+
+    $(imageCrystal4).attr("data-crystalvalue", generateRandomNumber(16, 18));
 
     let crystalValue = ($(this).attr("data-crystalvalue"));
     crystalValue = parseInt(crystalValue);
-  console.log($(this));
+    console.log($(this));
     counter += crystalValue;
 
     alert("New score: " + counter);
@@ -48,7 +48,7 @@ $("#allCrystal").on("click", function() {
     //   alert("You lose!!");
     // }
 
-  });
+});
 
 //each click add to total sum
 
